06548f7d0cb9cf92fc1488447f3ef88ca90d1f3d
kingsleywebwork <kingsleywebwork@gmail.com>
2025-03-18 10:46:00 +0100

    setting up pages template

---
+++ admin/includes/nav.php
@@ -0,0 +1,56 @@
+<div class="row gx-3">
+              <div class="col-md-3 col-sm-6 col-12">
+                <div class="bg-transparent-light rounded-1 mb-3 position-relative">
+                  <div class="p-3 text-white">
+                    <div class="mb-2">
+                      <i class="bi bi-bar-chart fs-1 lh-1"></i>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between">
+                      <h5 class="m-0 fw-normal">Sales</h5>
+                      <h3 class="m-0">3500</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6 col-12">
+                <div class="bg-transparent-light rounded-1 mb-3 position-relative">
+                  <div class="p-3 text-white">
+                    <div class="mb-2">
+                      <i class="bi bi-bag-check fs-1 lh-1"></i>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between">
+                      <h5 class="m-0 fw-normal">Orders</h5>
+                      <h3 class="m-0">2900</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6 col-12">
+                <div class="bg-transparent-light rounded-1 mb-3 position-relative">
+                  <div class="p-3 text-white">
+                    <div class="arrow-label">+18%</div>
+                    <div class="mb-2">
+                      <i class="bi bi-box-seam fs-1 lh-1"></i>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between">
+                      <h5 class="m-0 fw-normal">Invoices</h5>
+                      <h3 class="m-0">6500</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6 col-12">
+                <div class="bg-transparent-light rounded-1 mb-3 position-relative">
+                  <div class="p-3 text-white">
+                    <div class="arrow-label">+24%</div>
+                    <div class="mb-2">
+                      <i class="bi bi-bell fs-1 lh-1"></i>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between">
+                      <h5 class="m-0 fw-normal">Alerts</h5>
+                      <h3 class="m-0">7200</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
---
+++ admin/includes/side.php
@@ -0,0 +1,21 @@
+<div class="sidebarMenuScroll">
+    <!-- Sidebar menu starts -->
+    <ul class="sidebar-menu">
+        <li class="active current-page">
+            <a href="index.html">
+                <i class="bi bi-pie-chart"></i>
+                <span class="menu-text">Dashboard</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="analytics.html">
+                <i class="fs-3 bi bi-link"></i>
+                <span class="menu-text">Users</span>
+            </a>
+        </li>
+
+
+
+    </ul>
+</div>
