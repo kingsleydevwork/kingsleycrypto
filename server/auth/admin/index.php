4956bf22a372e831df765e1b17fbf0f7b585c7b8
kingsleywebwork <kingsleywebwork@gmail.com>
2025-03-21 11:27:14 +0100

    working on users

--- server/auth/admin/index.php
+++ server/auth/admin/index.php
@@ -1,26 +1,34 @@
 <?php
 
-use Dom\Mysql;
-
     $ip = $_SESSION['admin'];
     $sql = mysqli_query($connection, "SELECT * FROM `admin` WHERE `id` = $ip");
 
-    if (mysqli_num_rows($sql) > 0){
-
-        while($data = mysqli_fetch_assoc($sql)){
+    if (isset($_SESSION['admin'])){
 
 
-            $username =  $data['username'];
+        if (mysqli_num_rows($sql) > 0){
 
-
-
-
+            while($data = mysqli_fetch_assoc($sql)){
+    
+    
+                $username =  $data['username'];
+    
+    
+    
+    
+            }
+    
+    
+        }else{
+    
+            header("location:../index.php");
+    
+    
         }
-
 
     }else{
 
-        header("location:../auth/index.php");
+        header("location:../index.php");
 
 
     }
